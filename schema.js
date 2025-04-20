bfa14b2c071f2677145b5663158ae0b63a25c965
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-04-20 22:12:24 +0200

    add game

--- schema.js
+++ schema.js
@@ -32,6 +32,16 @@ export const typeDefs = `#graphql
         authors: [Author]
         author(id: ID): Author
     }
+
+    type Mutation {
+        addGame(game: AddGameInput): Game
+        deleteGame(id: ID!): [Game]
+    }
+
+    input AddGameInput{
+        title: String!,
+        platform: [String!]!
+    }
 `
 
 //int, float, string, boolean, ID 
