be2ecfb700ec72d8e36784c00d3485a6e96445b0
Sarker Sunzid Mahmud <sunzid02@gmail.com>
2025-04-20 20:51:33 +0200

    Related Data

--- index.js
+++ index.js
@@ -27,7 +27,29 @@ const resolvers = {
         author(_, args ){
             return db.authors.find((author) => author.id === args.id)
         }
+    },
+    
+    Game: {
+        reviews(parent){
+            return db.reviews.filter((rvw) => rvw.game_id === parent.id)
+        }
+    },
+        
+    Author: {
+        reviews(parent){
+            return db.reviews.filter((rvw) => rvw.author_id === parent.id)
+        }
+    },
+
+    Review: {
+        author(parent){
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent){
+            return db.games.find((g) => g.id === parent.game_id)
+        }
     }
+
 }
 
 
